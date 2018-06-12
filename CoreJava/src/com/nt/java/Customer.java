a103eb84df0ae40e7def543703efb1892fa0ac47
nikhil dasari <nikhildasari2015@gmail.com>
2018-06-12 16:16:46 +0530

    Commited changes

---
+++ CoreJava/src/com/nt/java/Customer.java
@@ -0,0 +1,26 @@
+
+package com.nt.java;
+
+public class Customer {
+	private int prodid;
+	private String name;
+	public int getProdid() {
+		return prodid;
+	}
+	public void setProdid(int prodid) {
+		this.prodid = prodid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Customer [prodid=" + prodid + ", name=" + name + "]";
+	}
+	
+	
+
+}
--- CoreJava/src/com/nt/java/helloworld.java
+++ CoreJava/src/com/nt/java/helloworld.java
@@ -4,6 +4,6 @@ public class helloworld {
 
 	public static void main(String[] args) {
 
-			System.out.println("Hi world!");
+			System.out.println("Hi java!");
 	}
 }
